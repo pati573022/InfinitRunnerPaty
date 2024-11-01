1c697408338f778d6eda789708b1293009956191
paty <patiangelita15@gmail.com>
2024-11-01 11:09:20 -0300

    paaaty

--- InfinitRunnerPaty/MainPage.xaml.cs
+++ InfinitRunnerPaty/MainPage.xaml.cs
@@ -2,23 +2,47 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+bool estaMorto=false;
+bool estaPulando=false;
+const int tempoEntreFrames=25;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+int velocidade1= 0;
+int velocidade2 = 0;
+int velocidade3 = 0;
+int velocidade=0;
+int larguraJanela=0;
+int alturaJanela=0;
+protected override void OnSizeAllocated(double w, double h)
+{
+	base.OnSizeAllocated(w, h);
+	CorrigeTamanhoCenario(w, h);
+	CalculaVelocidade(w);
+}
+void CalculaVelocidade CalculaVelocidade(double w)
+{
+	velocidade1 = (int)(w* 0.001);
+	velocidade2 = (int)(w* 0.004);
+	velocidade3 = (int)(w* 0.008);
+	velocidade=   (int)(w*0.001);
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+}
+void CorrigeTamanhoCenario (double w, double h)
+{
+	foreach (var a in HSLayer1.Children)
+	(a as Image).WidthRequest = w;
+	foreach (var a in HSLayer2.Children)
+	(a as Image).WidthRequest = w;
+	foreach (var a in HSLayer3.Children)
+	(a as Image).WidthRequest = w;
+	foreach(var a in HSLayerChao.Children)
+	(a as Image.).WidthRequest = w;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+	HSLayer1.WidthRequest = w
+	HSLayer2.WidthRequest = w*
+	HSLayer3.WidthRequest = w*
+	HSLayerChao.WidthRequest =w*
+}
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+
 }
 
